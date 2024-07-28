39badf35134dd58d57ef1f7e6b1e8d672c5ff5b5
Harsh-agrawal369 <agrawalharsh831@gmail.com>
2024-07-28 13:30:27 +0530

    gif added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
           className='w-full lg:w-1/2 lg:p-8'
         >
           <div className='flex items-center justify-center'>
-            <img className="rounded-2xl" src={aboutImg} alt="" />
+            <img className="rounded-2xl" src='/gif2.gif' alt="" />
           </div>
         </motion.div>
         <motion.div 

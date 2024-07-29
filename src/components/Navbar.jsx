8175db5d09e3468fef35257ab1527c8d5890de29
Harsh-agrawal369 <agrawalharsh831@gmail.com>
2024-07-29 13:28:03 +0530

    favicon added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center text-white text-bold text-xl">
-      {/* <p className="font-extrabold text-4xl logo-font">Ha</p> */}
+        {/* <img src="/favicon-32x32.png" alt="" /> */}
       </div>
 
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">

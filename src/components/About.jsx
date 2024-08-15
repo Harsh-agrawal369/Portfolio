66be0f3c279a9cc930a28f5597d435eea1c7dec3
Harsh-agrawal369 <agrawalharsh831@gmail.com>
2024-08-15 16:21:21 +0530

    New page opening problem resolved

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,50 +3,60 @@ import { ABOUT_TEXT } from "../constants";
 import { motion } from "framer-motion";
 
 const About = () => {
+  const openInNewTab = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
+
   return (
-    <div className='border-b border-neutral-900 pb-4'>
-      <h1 className='my-20 text-center text-4xl'>
+    <div className="border-b border-neutral-900 pb-4">
+      <h1 className="my-20 text-center text-4xl">
         About
-        <span className='text-neutral-500'> Me</span>  
+        <span className="text-neutral-500"> Me</span>
       </h1>
 
-      <div className='flex flex-wrap'>
-        <motion.div 
+      <div className="flex flex-wrap">
+        <motion.div
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: -100 }}
           transition={{ duration: 0.5 }}
-          className='w-full lg:w-1/2 lg:p-8'
+          className="w-full lg:w-1/2 lg:p-8"
         >
-          <div className='flex items-center justify-center overflow-hidden'>
-            <img 
-              className="rounded-2xl lg:w-25/40 w-5/5 object-cover object-bottom h-96" 
-              src='/img1.png' 
-              alt="About" 
+          <div className="flex items-center justify-center overflow-hidden">
+            <img
+              className="rounded-2xl lg:w-25/40 w-5/5 object-cover object-bottom h-96"
+              src="/img1.png"
+              alt="About"
             />
           </div>
         </motion.div>
-        <motion.div 
+        <motion.div
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: -100 }}
           transition={{ duration: 0.5 }}
           className="w-full lg:w-1/2"
         >
           <div className="flex flex-col items-center lg:items-start">
-            <p className="my-2 max-w-xl py-6 text-justify font-body-text">{ABOUT_TEXT}</p>
+            <p className="my-2 max-w-xl py-6 text-justify font-body-text">
+              {ABOUT_TEXT}
+            </p>
             <a
-              href='https://drive.google.com/file/d/14Lo_OJEF1x2nImw6qoZla1kHMBhT7ivI/view?usp=sharing'
-              download="Harsh_Agrawal_Resume.pdf"
+              href="https://drive.google.com/file/d/14Lo_OJEF1x2nImw6qoZla1kHMBhT7ivI/view?usp=sharing"
               type="application/pdf"
-              target="_blank"
+              onClick={(e) => {
+                e.preventDefault();
+                openInNewTab(
+                  "https://drive.google.com/file/d/14Lo_OJEF1x2nImw6qoZla1kHMBhT7ivI/view?usp=sharing"
+                );
+              }}
               className="mt-4 inline-block px-5 py-3 rounded-md bg-purple-800 text-white text-lg font-medium hover:bg-purple-600 transition-colors duration-300 font-fam"
             >
               Download CV
             </a>
           </div>
         </motion.div>
-      </div>      
+      </div>
     </div>
   );
-}
+};
 
 export default About;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,13 @@ import { FaEnvelope, FaLinkedin, FaGithub } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
 
 const Navbar = () => {
+
+  const openInNewTab = (url) => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
+
+
+
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center text-white text-bold text-xl">
@@ -12,22 +19,31 @@ const Navbar = () => {
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <a
           href={import.meta.env.VITE_LINKEDIN_URL}
-          target="_blank"
           rel="noopener noreferrer"
+          onClick={(e) => {
+            e.preventDefault();
+            openInNewTab(import.meta.env.VITE_LINKEDIN_URL);
+          }}
         >
           <FaLinkedin />
         </a>
         <a
           href={import.meta.env.VITE_GITHUB_URL}
-          target="_blank"
           rel="noopener noreferrer"
+          onClick={(e) => {
+            e.preventDefault();
+            openInNewTab(import.meta.env.VITE_GITHUB_URL);
+          }}
         >
           <FaGithub />
         </a>
         <a
           href={import.meta.env.VITE_LEETCODE_URL}
-          target="_blank"
           rel="noopener noreferrer"
+          onClick={(e) => {
+            e.preventDefault();
+            openInNewTab(import.meta.env.VITE_LEETCODE_URL);
+          }}
         >
           <SiLeetcode />
         </a>
